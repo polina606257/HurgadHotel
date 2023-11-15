919e90ef992e7bebc0915e9f6f4bfa959d2b6d91
polinas888 <p.shcherbinina@itransition.com>
2023-11-15 19:42:54 +0400

    modules setup

---
+++ domain/src/main/java/com/example/hurgadhotel/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.hurgadhotel.domain
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "HurgadHotel"
 include(":app")
- 
+include(":data")
+include(":domain")
