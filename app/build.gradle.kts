9246148b69d70d1004ee812a5b0fa716fd32ec7a
polinas888 <p.shcherbinina@itransition.com>
2023-11-20 14:05:44 +0400

    UI first screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,4 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
-    //material design
-    implementation ("com.google.android.material:material:1.10.0")
 }
--- app/src/main/java/com/example/hurgadhotel/hotelPage/HotelFragment.kt
+++ app/src/main/java/com/example/hurgadhotel/hotelPage/HotelFragment.kt
@@ -1,11 +1,15 @@
 package com.example.hurgadhotel.hotelPage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.hurgadhotel.R
 import com.example.hurgadhotel.databinding.FragmentHotelBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -23,10 +27,33 @@ class HotelFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        hotelViewModel.hotel.observe(viewLifecycleOwner)  { hotelInfo ->
+        hotelViewModel.hotel.observe(viewLifecycleOwner) { hotelInfo ->
             val photos = hotelInfo?.image_urls
             val adapter = photos?.let { PhotoCarouselAdapter(it) }
-            binding.hotelPhotoPager.adapter = adapter
+            binding.photoPager.adapter = adapter
+            createAndUpdateDotIndicator(photos)
+
+            binding.hotelName.text = hotelInfo?.name
+            binding.rating.text = hotelInfo?.rating.toString()
+            binding.ratingName.text = hotelInfo?.rating_name
+            binding.hotelAddress.text = hotelInfo?.adress
+            binding.price.text = "от ${hotelInfo?.minimal_price}р"
+            binding.priceInfo.text = hotelInfo?.price_for_it
+            binding.hotelDescription.text = hotelInfo?.hotelDetails?.description
+
+            for (peculiar in hotelInfo?.hotelDetails?.peculiarities!!) {
+                val peculiarView = LayoutInflater.from(context).inflate(
+                    R.layout.peculiar_item,
+                    binding.peculiarContainer,
+                    false
+                ).apply {
+                    id = View.generateViewId()
+                } as TextView
+
+                peculiarView.text = peculiar
+                binding.peculiarContainer.addView(peculiarView)
+                binding.peculiar.addView(peculiarView)
+            }
         }
 
         binding.toChooseNumberBtn.setOnClickListener {
@@ -34,6 +61,32 @@ class HotelFragment : Fragment() {
                 putString("hotelName", hotelViewModel.hotel.value?.name)
             }
             it.findNavController().navigate(R.id.action_hotelFragment_to_roomFragment, bundle)
+        }
+    }
+
+    private fun createAndUpdateDotIndicator(photos: ArrayList<String>?) {
+        if (photos?.indices != null) {
+            for (i in photos.indices) {
+                val dot = ImageView(requireContext())
+                dot.setImageResource(R.drawable.indicator_dot)
+                dot.isSelected = (i == 0)
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                params.setMargins(8, 0, 8, 0)
+                dot.layoutParams = params
+                binding.indicatorLayout.addView(dot)
+            }
+
+            binding.photoPager.registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    for (i in 0 until binding.indicatorLayout.childCount) {
+                        binding.indicatorLayout.getChildAt(i).isSelected = (i == position)
+                    }
+                }
+            })
         }
     }
 }
